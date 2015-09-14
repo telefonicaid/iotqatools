6ddbc3fae73963c8aab58ccde1460d6157550476
macs <macs@tid.es>
2015-09-14 11:33:23 +0200

    simulator for CA-thirdParties integration

---
+++ iotqatools/simulators/thirdPartySimulator.py
@@ -0,0 +1,74 @@
+#!/usr/bin/python
+
+__author__ = 'macs'
+
+from flask import Flask, request, Response
+from sys import argv
+
+app = Flask(__name__)
+
+# Default arguments
+port = 7010
+host = '0.0.0.0'
+
+# Arguments from command line
+if len(argv) > 1:
+    port = int(argv[1])
+
+
+@app.route('/sync/request', methods=['POST'])
+def treat_sync_request():
+    global lastRequest, myResponse, cont
+
+    cont += 1
+    lastRequest = request.data
+    resp = Response()
+    return Response(response=myResponse, status=200, content_type='application/json', headers=resp.headers)
+
+
+@app.route('/setResponseToError', methods=['GET'])
+def treat_set_response_to_error():
+    global responseError, myResponse
+
+    myResponse = '{"error": "GUH-1", "details": {"rgb": "CC0000", "t": 3}}'
+    return Response(response='Simulator set to Error', status=200)
+
+
+@app.route('/setResponseToOk', methods=['GET'])
+def treat_set_response_to_ok():
+    global responseError, myResponse
+
+    myResponse = '{"details": {"rgb":"66CC00","t":2}}'
+    return Response(response='Simulator set to OK', status=200)
+
+
+@app.route('/last', methods=['GET'])
+def treat_last():
+    global lastRequest
+
+    resp_last = Response()
+    return Response(response=lastRequest, status=200, content_type='application/json', headers=resp_last.headers)
+
+
+@app.route('/count', methods=['GET'])
+def count():
+    global cont
+    return Response(response=str(cont), status=200)
+
+
+@app.route('/reset', methods=['GET'])
+def reset():
+    global lastRequest, cont
+    lastRequest = ''
+    cont = 0
+    return Response(status=200)
+
+
+# Globals
+lastRequest = ''
+responseError = ''
+myResponse = '{"details": {"rgb":"66CC00","t":2}}'
+cont = 0
+
+if __name__ == '__main__':
+    app.run(host=host, port=port)
