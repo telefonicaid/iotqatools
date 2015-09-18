d12251a92791740ce1a1c89046d0461425376b24
macs <macs@tid.es>
2015-09-18 10:49:37 +0200

    added url callback functionality

--- iotqatools/simulators/thirdPartySimulator.py
+++ iotqatools/simulators/thirdPartySimulator.py
@@ -1,17 +1,21 @@
 #!/usr/bin/python
 
+
 __author__ = 'macs'
 
-from flask import Flask, request, Response
+from flask import Flask, request, Response, g
 from sys import argv
+from time import sleep
+import threading
 import random
 import json
+import requests
 
 app = Flask(__name__)
 
 # Default arguments
-port = 7010
-host = '0.0.0.0'
+port = 6500
+host = '127.0.0.1'
 
 # Arguments from command line
 if len(argv) > 1:
@@ -20,40 +24,62 @@ if len(argv) > 1:
 
 @app.route('/sync/request', methods=['POST'])
 def treat_sync_request():
-    global lastRequest, myResponse, cont
+    global lastRequest, sync_response, cont
 
-    my_result = {}
+    print "Recived sync request"
+    #Increase the number of requests
     cont += 1
+    #Store the last request
     lastRequest = request.data
-    jdata = json.loads(request.data)
-    my_result["result"] = 'action-' + jdata['action'] + ', extra-' + jdata['extra']
-    resp = Response()
-    return Response(response=json.dumps(my_result), status=200, content_type='application/json', headers=resp.headers)
+    print request.data
+    return Response(response=sync_response, status=200, content_type='application/json')
 
 
 @app.route('/async/create', methods=['POST'])
 def treat_async_create():
-    global asyncRequest
+    global lastRequest, async_request, url_callback, async_response
 
-    uid = str(random.randint(1, 999))
-    asyncRequest[uid] = '{"state": "P"}'
-    print asyncRequest
-    myresp = '{"id": ' + uid + ', "details": {"rgb" : "66CCDD", "t": 2}}'
-    return Response(response=myresp, status=200, content_type='application/json')
+    print "Recived async create"
+    #Store the last request
+    lastRequest = request.data
+    #generates the externalId
+    uid = str(random.randint(1, 9999999))
+    #Retrieve callback url and buttonId from request
+    url_callback = json.loads(request.data)['callbackUrl']
+    button_id = json.loads(request.data)['buttonId']
+    async_response['externalId'] = str(uid)
+    async_response['buttonId'] = str(button_id)
+    async_response['details'] = {}
+    async_response['details']['rgb'] = '66CCDD'
+    async_response['details']['t'] = '2'
+    print str(async_response)
+    #Invoke callback response
+    t = threading.Thread(target=invoke_ca)
+    t.start()
+    return Response(response='Create Received OK', status=200)
+
+
+def invoke_ca():
+    #Wait until request is finished
+    sleep(3)
+    #Send data to urlCallback
+    headers = {'Accept': 'application/json', 'content-type': 'application/json'}
+    r = requests.post(url_callback, data=json.dumps(async_response), headers=headers)
+    return r
 
 
 @app.route('/async/requests', methods=['POST','GET'])
 def treat_async_request_all():
-    global asyncRequest
+    global async_request
 
-    return Response(response=str(asyncRequest), status=200)
+    return Response(response=str(async_request), status=200)
 
 
 @app.route('/async/request/<uid>', methods=['POST','GET'])
 def treat_async_request(uid):
-    global asyncRequest
+    global async_request
 
-    return Response(response=asyncRequest[uid], status=200, content_type='application/json')
+    return Response(response=async_request[uid], status=200, content_type='application/json')
 
 
 @app.route('/setResponseToError', methods=['GET'])
@@ -76,8 +102,7 @@ def treat_set_response_to_ok():
 def treat_last():
     global lastRequest
 
-    resp_last = Response()
-    return Response(response=lastRequest, status=200, content_type='application/json', headers=resp_last.headers)
+    return Response(response=lastRequest, status=200)
 
 
 @app.route('/count', methods=['GET'])
@@ -88,9 +113,9 @@ def count():
 
 @app.route('/reset', methods=['GET'])
 def reset():
-    global lastRequest, asyncRequest, cont
+    global lastRequest, async_request, cont
     lastRequest = ''
-    asyncRequest = {}
+    async_request = {}
     cont = 0
     return Response(status=200)
 
@@ -98,9 +123,12 @@ def reset():
 # Globals
 lastRequest = ''
 responseError = ''
-myResponse = '{"details": {"rgb":"66CC00","t":2}}'
+sync_response = '{"details": {"rgb":"66CC00","t":2}}'
+async_response = {}
 cont = 0
-asyncRequest = {}
+async_request = {}
+url_callback = "http://localhost:9999"
+
 
 if __name__ == '__main__':
-    app.run(host=host, port=port)
+    app.run(host=host, port=port, debug=True)
