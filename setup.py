6740361042aa7931cbd87f1cb897e8ab6b687dc2
macs <macs@tid.es>
2015-10-02 12:44:00 +0200

    License amended

--- setup.py
+++ setup.py
@@ -2,18 +2,23 @@
 # -*- coding: utf-8 -*-
 """
 Copyright 2015 Telefonica Investigación y Desarrollo, S.A.U
-This file is part of fiware-orion-pep
-orchestrator is free software: you can redistribute it and/or
+
+This file is part of telefonica-iot-qa-tools
+
+iot-qa-tools is free software: you can redistribute it and/or
 modify it under the terms of the GNU Affero General Public License as
 published by the Free Software Foundation, either version 3 of the License,
 or (at your option) any later version.
-orchestrator is distributed in the hope that it will be useful,
+
+iot-qa-tools is distributed in the hope that it will be useful,
 but WITHOUT ANY WARRANTY; without even the implied warranty of
 MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
 See the GNU Affero General Public License for more details.
+
 You should have received a copy of the GNU Affero General Public
-License along with orchestrator.
+License along with iot-qa-tools.
 If not, seehttp://www.gnu.org/licenses/.
+
 For those usages not covered by the GNU Affero General Public License
 please contact with::[iot_support@tid.es]
 """
