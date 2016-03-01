2bb6a2bea31e6f608f85654e73a779335919850f
IVAN ARIAS LEON <ivan.ariasleon@telefonica.com>
2016-03-01 09:42:23 +0100

    [FEATURE]default value in types

--- setup.py
+++ setup.py
@@ -69,7 +69,7 @@ req_dev = get_requirements('requirements.txt')
 
 setup(
     name='iotqatools',
-    version='0.1.15',
+    version='0.1.16',
     description='Iot QA Tools',
     url='https://github.com/telefonicaid/iot-qa-tools',
     author='Telefonica I+D',
