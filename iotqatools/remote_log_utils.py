fc2c39570beb1556197d44a47a31049bda3d434f
IVAN ARIAS LEON <ivan.ariasleon@telefonica.com>
2016-03-21 09:55:19 +0100

    [FIX] license and library name in setup file

--- iotqatools/remote_log_utils.py
+++ iotqatools/remote_log_utils.py
@@ -1,27 +1,28 @@
 # -*- coding: utf-8 -*-
 """
- Copyright 2016 Telefonica Investigacion y Desarrollo, S.A.U
+Copyright 2016 Telefonica Investigación y Desarrollo, S.A.U
 
- This file is part of Orion Context Broker.
+This file is part of telefonica-iot-qa-tools
 
- Orion Context Broker is free software: you can redistribute it and/or
- modify it under the terms of the GNU Affero General Public License as
- published by the Free Software Foundation, either version 3 of the
- License, or (at your option) any later version.
+iot-qa-tools is free software: you can redistribute it and/or
+modify it under the terms of the GNU Affero General Public License as
+published by the Free Software Foundation, either version 3 of the License,
+or (at your option) any later version.
 
- Orion Context Broker is distributed in the hope that it will be useful,
- but WITHOUT ANY WARRANTY; without even the implied warranty of
- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero
- General Public License for more details.
+iot-qa-tools is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+See the GNU Affero General Public License for more details.
 
- You should have received a copy of the GNU Affero General Public License
- along with Orion Context Broker. If not, see http://www.gnu.org/licenses/.
+You should have received a copy of the GNU Affero General Public
+License along with iot-qa-tools.
+If not, seehttp://www.gnu.org/licenses/.
 
- For those usages not covered by this license please contact with
- iot_support at tid dot es
+For those usages not covered by the GNU Affero General Public License
+please contact with::[iot_support@tid.es]
 """
-
 __author__ = 'Iván Arias León (ivan dot ariasleon at telefonica dot com)'
+
 
 import os
 
